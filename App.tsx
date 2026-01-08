a93f41594cb79a6e42100254c9b1cfa8c014b9a3
Krucifer. <57034696+g336@users.noreply.github.com>
2026-01-08 18:01:23 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WebView } from 'react-native-webview';
+
+
 import LoginScreen from './screens/LoginScreen';
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import ResetPasswordScreen from './screens/ResetPasswordScreen';
 import VerifyCodeScreen from './screens/VerifyCodeScreen';
-import StudentHomeScreen from './screens/StudentHomeScreen';
+import StudentTabs from './navigation/StudentTabs';
+import PYQScreen from './screens/PYQScreen';
+import AttendanceScreen from './screens/AttendanceScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import LeaveRequestScreen from './screens/LeaveRequestScreen';
 
 export type RootStackParamList = {
   Login: undefined;
+  StudentTabs: {
+    enrollment: string;
+  }
   ForgotPassword: undefined;
   ResetPassword: undefined;
   VerifyCode: undefined;
-  StudentHome: undefined;
+  PYQ: undefined;
+  Attendance: undefined; 
+  Profile: undefined;
+  LeaveRequest: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -25,7 +38,15 @@ export default function App() {
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
         <Stack.Screen name="VerifyCode" component={VerifyCodeScreen} />
-        <Stack.Screen name="StudentHome" component={StudentHomeScreen} />
+
+        {/* AFTER LOGIN */}
+        <Stack.Screen name="StudentTabs" component={StudentTabs} />
+        
+        {/* NEW SCREENS */}
+        <Stack.Screen name="PYQ" component={PYQScreen} />
+        <Stack.Screen name="Attendance" component={AttendanceScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="LeaveRequest" component={LeaveRequestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
